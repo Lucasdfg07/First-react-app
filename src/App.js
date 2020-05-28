2b483524be25a4bd4d0ebf9eda4895d7c0593067
Lucas Siqueira Fernandes <lucassiqueirafernandes07@gmail.com>
2020-05-27 22:36:41 -0300

    setting lorem ipsum component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import HelloWorld from './components/hello_world';
+import LoremIpsum from './components/lorem_ipsum';
 
 function App() {
   return (
     <div>
       <HelloWorld/>
+      <LoremIpsum />
     </div>
   );
 }
